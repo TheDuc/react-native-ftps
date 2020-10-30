9ff1bd9612903d98614ef85e8b21ba8571ac4c7d
Igor Joshevski <gigo_j44@hotmail.com>
2020-10-30 14:26:06 +0100

    Initial commit with updates

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
-# react-native-ftps
-Library update over existing npm package react-native-ftp to support FTPS and TLS
+
+# react-native-ftp
+
+## Tested
+Currently tested to work on Android
+
+## Getting started
+
+`$ npm install react-native-ftps --save`
+
+### Mostly automatic installation
+
+`$ react-native link react-native-ftp`
+
+### Manual installation
+
+
+#### iOS
+
+1. In XCode, in the project navigator, right click `Libraries` ➜ `Add Files to [your project's name]`
+2. Go to `node_modules` ➜ `react-native-ftp` and add `RNFtp.xcodeproj`
+3. In XCode, in the project navigator, select your project. Add `libRNFtp.a` to your project's `Build Phases` ➜ `Link Binary With Libraries`
+4. Run your project (`Cmd+R`)<
+
+#### Android
+
+1. Open up `android/app/src/main/java/[...]/MainActivity.java`
+  - Add `import com.reactlibrary.RNFtpPackage;` to the imports at the top of the file
+  - Add `new RNFtpPackage()` to the list returned by the `getPackages()` method
+2. Append the following lines to `android/settings.gradle`:
+  	```
+  	include ':react-native-ftp'
+  	project(':react-native-ftp').projectDir = new File(rootProject.projectDir, 	'../node_modules/react-native-ftp/android')
+  	```
+3. Insert the following lines inside the dependencies block in `android/app/build.gradle`:
+  	```
+      compile project(':react-native-ftp')
+  	```
+
+#### Windows
+[Read it! :D](https://github.com/ReactWindows/react-native)
+
+1. In Visual Studio add the `RNFtp.sln` in `node_modules/react-native-ftp/windows/RNFtp.sln` folder to their solution, reference from their app.
+2. Open up your `MainPage.cs` app
+  - Add `using Com.Reactlibrary.RNFtp;` to the usings at the top of the file
+  - Add `new RNFtpPackage()` to the `List<IReactPackage>` returned by the `Packages` method
+
+
+## Usage
+```javascript
+
+import FTP from 'react-native-ftps';
+
+const testFTP = async () =>
+{
+  const host = "HOST";
+  const port = 990;
+  const tls = true; //TLS goes with 990 true always
+  
+  //check for active connection
+  let connected = await FTP.client_status().catch((error) => {return false});
+
+  if( connected == "connected" )
+  await logout(); //Close previous connection
+
+  console.log('Make new connection'); 
+  FTP.setup(host, port, tls ) 
+  FTP.login("demo","password") 
+  .then(async (result) => 
+  {
+    console.log(result, await FTP.client_status()); 
+    FTP.list("./").then(
+        (result)=>
+        {
+          console.log('Listing', result);
+          setTimeout( async () => {
+            await logout(); 
+          }, 2000);
+        })
+        .catch((e) => {
+        console.log('ERR', e)
+      });
+  },
+  (error) => 
+  {
+    console.log(error);
+  })
+  .catch(async (error) => 
+  {
+	console.log('Catch', error);
+  })
+}
+
+const disconnectFTP = async () => {
+  let connected = await FTP.client_status().catch((error) => {return false});
+
+  if(connected == "connected")
+  await logout(); 
+  // else
+  // FTP.force_disconnect();
+}
---
+++ android/src/main/java/com/reactlibrary/RNFtpModule.java
@@ -0,0 +1,287 @@
+
+package com.reactlibrary;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPSClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+import org.apache.commons.net.util.TrustManagerUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class RNFtpModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+  private static String ip_address;
+  private static int port;
+  private static FTPSClient client;
+  private static boolean is_tls;
+
+  public RNFtpModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @ReactMethod
+  public void setup(String ip_address, int port, boolean is_tls){
+    this.is_tls = is_tls;
+    this.ip_address = ip_address;
+    this.port = port;
+
+    if(this.is_tls == true)
+    {
+      this.client = new FTPSClient("TLS", true); //Will force the use of TLS - implicit
+    }
+    else
+    {
+      this.client = new FTPSClient(); //Will use explicit
+    }
+  }
+
+  @ReactMethod
+  public void client_status(final Promise promise){
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        if(client == null)
+        promise.resolve("disconnected");
+        else
+        promise.resolve("connected");
+      }
+    }).start();
+  }
+
+  @ReactMethod
+  public void login(final String username, final String password, final Promise promise){
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          
+          client.setRemoteVerificationEnabled(false);
+          client.setTrustManager(TrustManagerUtils.getAcceptAllTrustManager()); 
+          //client.setControlEncoding("UTF-8")
+          client.connect(RNFtpModule.this.ip_address,RNFtpModule.this.port);
+          client.setSoTimeout(2000);
+
+          if ( ! FTPReply.isPositiveCompletion(client.getReplyCode()) ) {
+
+            client.disconnect();
+            throw new Exception("FTP Haven't replied");
+          }
+
+          client.enterLocalPassiveMode();
+          
+          if(client.login(username, password))
+          {
+            // logger.debug("Logged successfully on FTP");
+            promise.resolve(true);
+          }
+          else
+          {
+            // logger.debug("Can't login to FTP");
+            promise.reject("ERROR", "Can't login to FTP");
+          }
+          
+        } catch (Exception e) {
+          promise.reject("ERROR",e.getMessage());
+        }
+      }
+    }).start();
+  }
+
+  @ReactMethod
+  public void list(final String path, final Promise promise){
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        FTPFile[] files = new FTPFile[0];
+        try {
+          files = client.listFiles(path);
+          JSONObject json = new JSONObject();
+          JSONArray arrfiles = new JSONArray();
+          for (FTPFile file : files) {
+            JSONObject tmp = new JSONObject();
+            tmp.put("name",file.getName());
+            tmp.put("size",file.getSize());
+            tmp.put("timestamp",file.getTimestamp());
+            arrfiles.put(tmp);
+          }
+          json.put("results",arrfiles);
+          promise.resolve(json.toString());
+        } catch (Exception e) {
+          promise.reject("ERROR", e.getMessage());
+        }
+      }
+    }).start();
+  }
+
+  @ReactMethod
+  public void makedir(final String path, final Promise promise){
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          client.makeDirectory(path);
+          promise.resolve(true);
+        } catch (IOException e) {
+          promise.reject("ERROR",e.getMessage());
+        }
+      }
+    }).start();
+  }
+
+  @ReactMethod
+  public void removedir(final String path, final Promise promise){
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          client.removeDirectory(path);
+          promise.resolve(true);
+        } catch (IOException e) {
+          promise.reject("ERROR",e.getMessage());
+        }
+      }
+    }).start();
+  }
+  @ReactMethod
+  public void removeFile(final String path, final Promise promise){
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          client.deleteFile(path);
+          promise.resolve(true);
+        } catch (IOException e) {
+          promise.reject("ERROR",e.getMessage());
+        }
+      }
+    }).start();
+  }
+
+  @ReactMethod
+  public void changeDirectory(final String path, final Promise promise){
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          client.changeWorkingDirectory(path);
+          promise.resolve(true);
+        } catch (IOException e) {
+          promise.reject("ERROR",e.getMessage());
+        }
+      }
+    }).start();
+  }
+
+
+  @ReactMethod
+  public void logout(final Promise promise){
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {         
+          client.logout();
+          client.disconnect();
+          promise.resolve(true);
+        } catch (IOException e) {
+          promise.reject("ERROR",e.getMessage());
+        }
+      }
+    }).start();
+  }
+
+  @ReactMethod
+  public void force_disconnect(final Promise promise){
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try
+        {
+          client.disconnect();
+          promise.resolve(true);
+        }
+        catch(IOException e)
+        {
+          promise.reject("ERROR", e.getMessage());
+        }
+      }
+    }).start();
+  }
+
+
+  @ReactMethod
+  public void uploadFile(final String path,final String remoteDestinationDir, final Promise promise){
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          client.setFileType(FTP.BINARY_FILE_TYPE);
+          File firstLocalFile = new File(path);
+
+          String firstRemoteFile = remoteDestinationDir+"/"+firstLocalFile.getName();
+          InputStream inputStream = new FileInputStream(firstLocalFile);
+
+          System.out.println("Start uploading first file");
+          boolean done = client.storeFile(firstRemoteFile, inputStream);
+          inputStream.close();
+          if (done) {
+            promise.resolve(true);
+          }else{
+            promise.reject("FAILED",firstLocalFile.getName()+" is not uploaded successfully.");
+          }
+        } catch (IOException e) {
+          promise.reject("ERROR",e.getMessage());
+        }
+      }
+    }).start();
+  }
+
+
+  @ReactMethod
+  public void downloadFile(final String remoteFile1,final String localDestinationDir, final Promise promise){
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          client.setFileType(FTP.BINARY_FILE_TYPE);
+          File remoteFile = new File(remoteFile1);
+          File downloadFile1 = new File(localDestinationDir+"/"+remoteFile.getName());
+          OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
+          boolean success = client.retrieveFile(remoteFile1, outputStream1);
+          outputStream1.close();
+
+          if (success) {
+            promise.resolve(true);
+          }else{
+            promise.reject("FAILED",remoteFile.getName()+" is not downloaded successfully.");
+          }
+        } catch (IOException e) {
+          promise.reject("ERROR",e.getMessage());
+        }
+      }
+    }).start();
+  }
+
+
+  @Override
+  public String getName() {
+    return "FTP";
+  }
+}
